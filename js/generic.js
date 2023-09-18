394edf66edf32dfad8f542fa02d3fd0e369a1d7f
edme88 <edmealiciardi@gmail.com>
2023-09-18 01:41:49 -0300

    Add js file

---
+++ js/generic.js
@@ -0,0 +1,9 @@
+/** Login del usuario en la página principal
+ * @method login
+ * @param user {string} - Nombre de usuario
+ * @param pass {string} - Password del usuario
+ * */
+login = (user, pass) => {
+    //Hagamos de cuenta que el admin hizp login
+    window.location.href = "nuevoTkd.html";
+}
